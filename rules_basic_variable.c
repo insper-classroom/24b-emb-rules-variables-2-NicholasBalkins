cea987e1b810beb6eda28b9bd3b6a1c124e7fd73
NicholasBalkins <103457226+NicholasBalkins@users.noreply.github.com>
2024-08-18 17:17:46 -0300

    Update2 rules_basic_variable.c

--- rules_basic_variable.c
+++ rules_basic_variable.c
@@ -2,16 +2,16 @@
 
 // ..
 
-int a = 0; // var global
-int b = 0; // var global
-
-void foo(void) {
-    a = a + 1; // acessa variavel global
+void foo(int *a) {
+    *a = *a + 1;
 }
 
 int main(void) {
+    int a = 0; // Variável local
+    int b = 0; // Variável local
+    
     while (1) {
-        foo();
+        foo(&a);
         
         if (a > 5) {
             b = 1;
